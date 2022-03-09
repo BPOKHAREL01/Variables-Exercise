7d22cb696105684bcf0e0ccf59e4adb6130bf0d8
“bpokharel01” <bibhasa.pokharel@gmail.com>
2022-03-09 15:36:25 -0500

    Variables Exercise Uploaded

---
+++ script.js
@@ -0,0 +1,49 @@
+const age=21;
+
+let num=20;
+
+num=15;
+ console.log(15);
+ //OR
+ //var num=20;
+
+num=num+7;
+console.log(num);
+//num+=7;
+//OR
+//var num=15;
+//num=num+7;
+
+ num=num--;
+ console.log(num);
+ //OR
+ //var num=22;
+ //num=num-1;
+ //console.log(num);
+
+ num="This is not a number";
+ //console.log(num);
+
+ let blank;
+ //let blank="";
+
+ console.log(blank);
+ //let blank;
+ 
+ blank=null;
+
+ // BONUS //
+
+const quarter=0.25, dime=0.10, nickel=0.05, penny=0.01;
+//or
+//const [quarter, dime, nickel, penny];
+
+console.log(quarter, dime, nickel, penny);
+
+let letters="qwertyuiop";
+
+console.log(letters.length);//10
+console.log(letters[9]);
+
+console.log(letters.slice(-1));
+console.log(letters[letters.length -1]);
